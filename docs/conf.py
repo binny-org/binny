b88f4310905263c6bb90184eae2b76838e3fb103
Matthijs van der Wild <matthijs@vanderwild.com>
2026-01-03 12:55:56 -0500

    Add Sphinx support for Google style docstring documentation

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,12 @@ author = 'Nikolina Šarčević'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+autoclass_content = "both"
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
